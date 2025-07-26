89ff7b731c1728197727c9c15223afba553b5c1c
rami-hardware <killx51@hotmail.com>
2025-07-26 08:33:29 +0300

    gateway finished -> media

--- main.tf
+++ main.tf
@@ -46,3 +46,50 @@ resource "proxmox_vm_qemu" "gateway" {
   }
 
 }
+
+resource "proxmox_vm_qemu" "media" {
+
+  name        = "media-server"
+  target_node = "server"
+  clone       = "ubuntu-cloud-init-template"
+  full_clone = true
+  agent   = 1
+  os_type = "cloud-init"
+  vmid    = 911
+
+  ciuser     = "media"
+  cipassword = "media123"
+  ipconfig0  = "ip=192.168.1.201/24,gw=192.168.1.250"
+  
+  # Add VirtIO SCSI controller
+  scsihw = "virtio-scsi-pci"
+
+  cores   = 2
+  sockets = 1
+  memory  = 4096
+
+  disk {
+    type    = "cloudinit"
+    storage = "local-lvm"
+    slot    = "ide2" 
+  }
+
+  disk {
+    type    = "disk"
+    storage = "media"
+    size    = "3500G"
+    slot    = "scsi0" 
+  }
+
+  network {
+    id     = 0
+    model  = "virtio"
+    bridge = "vmbr0"
+  }
+   sshkeys = file("~/.ssh/id_rsa.pub")
+
+  lifecycle {
+    ignore_changes = [disk[0].size]
+  }
+
+}
